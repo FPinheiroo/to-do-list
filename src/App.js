ca5c8fd13f7eb196efd297354e3f4c8ad59db1a6
Felipe Pinheiro <feliperochapinheiro@gmail.com>
2024-07-26 18:18:38 -0300

    Add_buttons(no working)

--- src/App.js
+++ src/App.js
@@ -2,28 +2,34 @@ import { useState } from "react";
 import "./styles.css";
 
 function App() {
-  const [iteml,setItemL]=useState([]);
-  const [inputT,setinputT]=useState("");
+  const [iteml, setItemL] = useState([]);
+  const [inputT, setinputT] = useState("");
 
-
-  function handleAddItem(){
-    setItemL([...iteml,inputT]);
+  function handleAddItem() {
+    setItemL([...iteml, inputT]);
     setinputT("");
   }
 
   return (
-    <div class="container">
-      <input placeholder="Digite sua tarefa aqui !" id="to_do" type="text" value={inputT} onChange={(e)=> setinputT(e.target.value)}/>
-      <button id="add" onClick={handleAddItem}>Adicionar</button>
-      <select id="filter">
-        <option>Todas as Tarefas</option>
-        <option>Data</option>
-        <option>Concluidas</option>
-        <option>Pendentes</option>
-      </select>
-      <ul className="lista_a_fazer">
-        {iteml.map((item) => (
-          <li className="item">{item}</li>
+    <div className="container">
+      <div class="t">
+        <input placeholder="Digite sua tarefa aqui!" id="to_do" type="text" value={inputT} onChange={(e) => setinputT(e.target.value)}/>
+        <button id="add" onClick={handleAddItem}>Adicionar</button>
+        <select id="filter">
+          <option>Todas as Tarefas</option>
+          <option>Data</option>
+          <option>Concluidas</option>
+          <option>Pendentes</option>
+        </select>
+      </div>
+      <ul className="lista_a_fazer">{iteml.map((item, i) => (
+          <li className="item" key={i}>
+            {item}
+            <div className="item-buttons">
+              <button className="del_task">❌</button>
+              <button className="edit_task">✏️</button>
+            </div>
+          </li>
         ))}
       </ul>
     </div>
