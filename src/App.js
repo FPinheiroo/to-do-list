bb50885d20f58365d263e705497c8ffe626808eb
Felipe Pinheiro <feliperochapinheiro@gmail.com>
2024-07-26 17:21:53 -0300

    Add_to_list

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import { useState } from "react";
 import "./styles.css";
 
 function App() {
+  const [iteml,setItemL]=useState([]);
+  const [inputT,setinputT]=useState("");
+
+
+  function handleAddItem(){
+    setItemL([...iteml,inputT]);
+    setinputT("");
+  }
+
   return (
     <div class="container">
-      <input placeholder="Digite sua tarefa aqui !" id="to_do" type="text" />
-      <button id="add">Adicionar</button>
+      <input placeholder="Digite sua tarefa aqui !" id="to_do" type="text" value={inputT} onChange={(e)=> setinputT(e.target.value)}/>
+      <button id="add" onClick={handleAddItem}>Adicionar</button>
       <select id="filter">
         <option>Todas as Tarefas</option>
         <option>Data</option>
         <option>Concluidas</option>
         <option>Pendentes</option>
       </select>
+      <ul className="lista_a_fazer">
+        {iteml.map((item) => (
+          <li className="item">{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
