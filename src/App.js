be7271c95097d9bdb300e880aae3421297a405ab
Felipe Pinheiro <feliperochapinheiro@gmail.com>
2024-07-27 12:41:28 -0300

    add description input(working)

--- src/App.js
+++ src/App.js
@@ -4,28 +4,35 @@ import "./styles.css";
 function App() {
   const [iteml, setItemL] = useState([]);
   const [inputT, setinputT] = useState("");
+  const [inputTD, setinputTD] = useState("");
 
   function handleAddItem() {
-    const textovazio = inputT.trim()
-    if (textovazio == "") return;
+    const textovazio = inputT.trim();
+    const descricaovazia = inputTD.trim();
+    if (textovazio === "" || descricaovazia === "") return;
 
     let newItem = {
-      id:Date.now(),
+      id: Date.now(),
       itemLista: textovazio,
-    }
+      itemDescricao: descricaovazia
+    };
 
     setItemL([...iteml, newItem]);
     setinputT("");
+    setinputTD("");
   }
 
-  function handleExcluirItem(item){
-    setItemL((prevList) => prevList.filter((lista)=> lista != item));
+  function handleExcluirItem(item) {
+    setItemL((prevList) => prevList.filter((lista) => lista.id !== item.id));
   }
 
   return (
     <div className="container">
       <div class="t">
         <input placeholder="Digite sua tarefa aqui!" id="to_do" type="text" value={inputT} onChange={(e) => setinputT(e.target.value)}/>
+        <input placeholder="Descrição aqui!" id="to_do_desc" type="text" value={inputTD} onChange={(e) => setinputTD(e.target.value)}/>
+        </div>
+        <div class="t2">
         <button id="add" onClick={handleAddItem}>Adicionar</button>
         <select id="filter">
           <option>Todas as Tarefas</option>
@@ -34,9 +41,13 @@ function App() {
           <option>Pendentes</option>
         </select>
       </div>
-      <ul className="lista_a_fazer">{iteml.map((item) => (
+      <ul className="lista_a_fazer">
+        {iteml.map((item) => (
           <li className="item" key={item.id}>
-            {item.itemLista}
+            <div>
+              <strong>{item.itemLista}</strong>
+              <p id="desc">{item.itemDescricao}</p>
+            </div>
             <div className="item-buttons">
               <button className="del_task" onClick={() => handleExcluirItem(item)}>❌</button>
               <button className="edit_task">✏️</button>
